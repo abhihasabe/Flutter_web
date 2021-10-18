a36e923775156ed0a08a8d1228669bf806cfe713
Mayuri <mobilityfd@gmail.com>
2021-10-18 09:56:36 +0530

    Help Desk Structure

---
+++ lib/screens/helpdesk/ui/helpdesk_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class helpdesk_screen extends StatefulWidget {
+  const helpdesk_screen({Key? key}) : super(key: key);
+
+  @override
+  _helpdesk_screenState createState() => _helpdesk_screenState();
+}
+
+class _helpdesk_screenState extends State<helpdesk_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
