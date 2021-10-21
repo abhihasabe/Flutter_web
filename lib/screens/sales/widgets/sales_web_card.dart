7252484c7f71459ef10003bfc7ab0d7ac4373df4
admin <mobilityfd@gmail.com>
2021-10-21 16:59:23 +0530

    Inital Comment

--- lib/screens/sales/widgets/sales_web_card.dart
+++ lib/screens/sales/widgets/sales_web_card.dart
@@ -31,126 +31,30 @@ class _WebCardState extends State<WebCard> with TickerProviderStateMixin {
   }
 
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      elevation: 3,
-      margin: const EdgeInsets.only(left: 60.0, right: 60.0, bottom: 20.0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 0.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, top: 4),
-                        child: Text(
-                          'Order No',
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 160.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, top: 4),
-                        child: Text(
-                          'ORDER PLACED',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 110.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, top: 4),
-                        child: Text(
-                          'Status',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 110.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, top: 4),
-                        child: Text(
-                          'Total Items',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 110.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, top: 4),
-                        child: Text(
-                          'Total',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 110.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, top: 4),
-                        child: Text(
-                          'View',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 0.0, top: 20.0),
-              child: Row(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        color: Colors.white,
+        elevation: 3,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(left: 1.0),
+                    padding: const EdgeInsets.only(left: 0.0),
                     child: Row(
                       children: <Widget>[
                         Padding(
                           padding: const EdgeInsets.only(left: 0.0, top: 4),
                           child: Text(
-                            '406-9338089-364435555',
+                            'Order No',
                             style: TextStyle(color: Colors.black, fontSize: 16),
                           ),
                         ),
@@ -158,13 +62,13 @@ class _WebCardState extends State<WebCard> with TickerProviderStateMixin {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 50.0),
+                    padding: const EdgeInsets.only(left: 160.0),
                     child: Row(
                       children: <Widget>[
                         Padding(
                           padding: const EdgeInsets.only(left: 0.0, top: 4),
                           child: Text(
-                            '10 Aug 2021',
+                            'ORDER PLACED',
                             textAlign: TextAlign.left,
                             style: TextStyle(color: Colors.black, fontSize: 16),
                           ),
@@ -179,22 +83,7 @@ class _WebCardState extends State<WebCard> with TickerProviderStateMixin {
                         Padding(
                           padding: const EdgeInsets.only(left: 0.0, top: 4),
                           child: Text(
-                            'Order Placed',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.black, fontSize: 16),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 130.0),
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(left: 0.0, top: 4),
-                          child: Text(
-                            '11',
+                            'Status',
                             textAlign: TextAlign.left,
                             style: TextStyle(color: Colors.black, fontSize: 16),
                           ),
@@ -209,7 +98,7 @@ class _WebCardState extends State<WebCard> with TickerProviderStateMixin {
                         Padding(
                           padding: const EdgeInsets.only(left: 0.0, top: 4),
                           child: Text(
-                            '\$10.299',
+                            'Total Items',
                             textAlign: TextAlign.left,
                             style: TextStyle(color: Colors.black, fontSize: 16),
                           ),
@@ -222,20 +111,153 @@ class _WebCardState extends State<WebCard> with TickerProviderStateMixin {
                     child: Row(
                       children: <Widget>[
                         Padding(
-                            padding: const EdgeInsets.only(left: 0.0, top: 4),
-                            child: Icon(
-                              Icons.remove_red_eye,
-                              color: Colors.blue,
-                            )),
+                          padding: const EdgeInsets.only(left: 0.0, top: 4),
+                          child: Text(
+                            'Total',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 110.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 0.0, top: 4),
+                          child: Text(
+                            'View',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Row(
+              Padding(
+                padding: const EdgeInsets.only(left: 0.0, top: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 1.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 0.0, top: 4),
+                            child: Text(
+                              '406-9338089-364435555',
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 0.0, top: 4),
+                            child: Text(
+                              '10 Aug 2021',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 110.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 0.0, top: 4),
+                            child: Text(
+                              'Order Placed',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 130.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 0.0, top: 4),
+                            child: Text(
+                              '11',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 110.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 0.0, top: 4),
+                            child: Text(
+                              '\$10.299',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 110.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                              padding: const EdgeInsets.only(left: 0.0, top: 4),
+                              child: Icon(
+                                Icons.remove_red_eye,
+                                color: Colors.blue,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 0.0, top: 2),
+                            child: Text(
+                              'Cancellation Reason',
+                              style: TextStyle(color: Colors.black, fontSize: 14),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -246,8 +268,8 @@ class _WebCardState extends State<WebCard> with TickerProviderStateMixin {
                         Padding(
                           padding: const EdgeInsets.only(left: 0.0, top: 2),
                           child: Text(
-                            'Cancellation Reason',
-                            style: TextStyle(color: Colors.black, fontSize: 14),
+                            'Expected Delivery Date Has Changed And The Product Is Arriving At A Later Date',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
                           ),
                         ),
                       ],
@@ -255,28 +277,8 @@ class _WebCardState extends State<WebCard> with TickerProviderStateMixin {
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, top: 2),
-                        child: Text(
-                          'Expected Delivery Date Has Changed And The Product Is Arriving At A Later Date',
-                          style: TextStyle(color: Colors.black, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
