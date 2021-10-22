4e6364e14215509d01aa87ad9a1cdfba5d3f0b75
admin <mobilityfd@gmail.com>
2021-10-22 11:47:19 +0530

    Inital Comment

--- lib/screens/dashboard/ui/trends_screen.dart
+++ lib/screens/dashboard/ui/trends_screen.dart
@@ -12,8 +12,8 @@ class Trends extends StatefulWidget {
 }
 
 class _Trends extends State<Trends> {
-  late bool isShowingMainData;
-
+  bool? isShowingMainData;
+  bool? displayMobileLayout;
   @override
   void initState() {
     super.initState();
@@ -22,7 +22,9 @@ class _Trends extends State<Trends> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    displayMobileLayout= MediaQuery.of(context).size.width < 600;
+    return !displayMobileLayout!?
+    Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
@@ -51,7 +53,7 @@ class _Trends extends State<Trends> {
                 ),
                 Expanded(
                   child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
+                    scrollDirection: !displayMobileLayout!?Axis.horizontal:Axis.vertical,
                     shrinkWrap: true,
                     itemCount: 3,
                     itemBuilder: _trendListBuilder,
@@ -77,7 +79,7 @@ class _Trends extends State<Trends> {
                 ),
                 Expanded(
                   child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
+                    scrollDirection: !displayMobileLayout!?Axis.horizontal:Axis.vertical,
                     shrinkWrap: true,
                     itemCount: 3,
                     itemBuilder: _statisticsListBuilder,
@@ -85,7 +87,7 @@ class _Trends extends State<Trends> {
                 ),
                 Expanded(
                   child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
+                    scrollDirection: !displayMobileLayout!?Axis.horizontal:Axis.vertical,
                     shrinkWrap: true,
                     itemCount: 3,
                     itemBuilder: _statisticsListBuilder,
@@ -171,7 +173,151 @@ class _Trends extends State<Trends> {
             )),
         SizedBox(width: 20,)
       ],
-    );
+    ):
+    Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+      SizedBox(
+        height: 2150,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 18.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "TRENDS",
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.cyan,
+                        fontWeight: FontWeight.bold),
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: !displayMobileLayout!?Axis.horizontal:Axis.vertical,
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: _trendListBuilder,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 18.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "STATISTICS",
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.cyan,
+                        fontWeight: FontWeight.bold),
+                  )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: !displayMobileLayout!?Axis.horizontal:Axis.vertical,
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: _statisticsListBuilder,
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: !displayMobileLayout!?Axis.horizontal:Axis.vertical,
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: _statisticsListBuilder,
+              ),
+            ),
+          ],
+        ),
+      ),
+      Column(
+        children: [
+          SizedBox(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left:12.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only( top: 8),
+                                child: Text("NOTIFICATION",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.cyan,
+                                      fontWeight: FontWeight.bold),
+
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(right:12.0, top: 8),
+                                child: Icon(Icons.notifications_active_outlined, color: Colors.cyan,),
+                              )
+                            ]),
+                        SizedBox(height: 10,),
+                        Text("16 sep 2021"),
+                        Text("John Deo replied on your ticket"),
+                        Divider(),
+                        Text("16 sep 2021"),
+                        Text("John Deo replied on your ticket"),
+                        Divider(),
+                        Text("16 sep 2021"),
+                        Text("John Deo replied on your ticket"),
+                        Divider()
+                      ]),
+                ),
+              )),
+          SizedBox(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left:8.0, bottom: 8),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10,),
+                        Text("ESSENTIAL TIPS", style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.cyan,
+                            fontWeight: FontWeight.bold)),
+                        SizedBox(height: 10,),
+                        Text("John Deo replied on your ticket. John Deo replied on your ticket. "
+                            "John Deo replied on your ticket.John Deo replied on your ticket"),
+                        SizedBox(height: 30),
+                        Padding(
+                          padding: const EdgeInsets.only(right:8.0),
+                          child: Align(child: Icon(Icons.settings, size: 58, color: Colors.grey), alignment: Alignment.centerRight),
+                        )
+                      ]),
+                ),
+              )),
+        ],
+      ),
+      SizedBox(width: 20,)
+    ]);
   }
 
   Widget _trendListBuilder(BuildContext context, int index) {
@@ -183,7 +329,7 @@ class _Trends extends State<Trends> {
         borderRadius: BorderRadius.circular(15.0),
       ),
       child: Container(
-        height: 100,
+        height: !displayMobileLayout!?100:240,
         child: Column(children: [
           SizedBox(
             height: 8,
